af3bcb2d61b63349a99ec9f59afb05b7956c8175
Fazmin Nizam <fazmin.nizam@gmail.com>
2022-05-17 23:19:06 -0400

    Create create_horizon_pools.ps1

---
+++ create_horizon_pools.ps1
@@ -0,0 +1,20 @@
+# Automate VMWare desktop Creation process
+# Tested - vCenter 7.2
+# for more indepth information please visit https://github.com/vmware/PowerCLI-Example-Scripts/tree/master/Modules/VMware.Hv.Helper.
+# Custmized script written by: Fazmin Nizam
+# This will create pools based on the Selected golden images make sure to add the required data prior executingt the script.
+
+#Credentials
+$Cred = Get-Credential
+
+#Global Settings
+$HVServer = "studentvm.engineerad.ca"
+$Vcenter = "vcsa2.engineerad.ca"
+$datacenter = "<Your Datacenter>"
+$datastore = "<Your Datsastore>"
+$DomainAdmin = "<Your Instant Clone Domain Admin>"
+$HostOrCluster = "<Your Cluster>"
+$ResourcePool = "<Resourcepool path>"
+$NetBiosName = "<Your NETBIOS>"
+$ParentVM = "<Name of ParentVM>"
+$SnapshotVM = "<Name of Snapshot VM>"
